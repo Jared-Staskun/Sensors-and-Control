0bd448f9de961d296c5850d598f4f5bc57901e80
Jared-Staskun <jared.staskun@gmail.com>
2022-05-10 18:33:25 +1000

    video demo

--- Image_processing.m
+++ Image_processing.m
@@ -1,4 +1,5 @@
 %% Intialise
+rosshutdown;
 rosinit;
 
 sub = rossubscriber("camera/color/image_raw");
@@ -8,7 +9,7 @@ image = readImage(msg2);
 
 %% Main
 
-% while(1)
+while(1)
 [msg2] = receive(sub);
 image = readImage(msg2);
 
@@ -26,43 +27,43 @@ green_centre = calculate_centroid(extracted_green);
 extracted_yellow = extract_yellow(image);
 yellow_centre = calculate_centroid(extracted_yellow);
 
-%find centre of purple cube
-extracted_purple = extract_purple(image);
-purple_centre = calculate_centroid(extracted_purple);
+%find centre of blue cube
+extracted_blue = extract_blue(image);
+blue_centre = calculate_centroid(extracted_blue);
 
 %plot results
 imshow(image);
 hold on
 
-scatter(red_centre.Centroid(1), red_centre.Centroid(2),'MarkerFaceColor','black','MarkerEdgeColor','black');
-red_u = num2str(round(red_centre.Centroid(1)));
-red_v = num2str(round(red_centre.Centroid(2)));
-red_info = append(red_u,", ",red_v);
-text(red_centre.Centroid(1) + 10, red_centre.Centroid(2), 'red','FontSize',14,'FontWeight','bold');
-text(red_centre.Centroid(1) + 10, red_centre.Centroid(2) + 25, red_info,'FontSize',14,'FontWeight','bold');
+    scatter(red_centre.Centroid(1), red_centre.Centroid(2),'MarkerFaceColor','black','MarkerEdgeColor','black');
+    red_u = num2str(round(red_centre.Centroid(1)));
+    red_v = num2str(round(red_centre.Centroid(2)));
+    red_info = append(red_u,", ",red_v);
+    text(red_centre.Centroid(1) + 10, red_centre.Centroid(2), 'RED','FontSize',14,'FontWeight','bold');
+    text(red_centre.Centroid(1) + 10, red_centre.Centroid(2) + 25, red_info,'FontSize',14,'FontWeight','bold');
 
-scatter(green_centre.Centroid(1), green_centre.Centroid(2),'MarkerFaceColor','black','MarkerEdgeColor','black');
-green_u = num2str(round(green_centre.Centroid(1)));
-green_v = num2str(round(green_centre.Centroid(2)));
-green_info = append(green_u,", ",green_v);
-text(green_centre.Centroid(1) + 10, green_centre.Centroid(2) + 25, green_info,'FontSize',14,'FontWeight','bold');
-text(green_centre.Centroid(1) + 10, green_centre.Centroid(2),'green','FontSize',14,'FontWeight','bold');
+    scatter(green_centre.Centroid(1), green_centre.Centroid(2),'MarkerFaceColor','black','MarkerEdgeColor','black');
+    green_u = num2str(round(green_centre.Centroid(1)));
+    green_v = num2str(round(green_centre.Centroid(2)));
+    green_info = append(green_u,", ",green_v);
+    text(green_centre.Centroid(1) + 10, green_centre.Centroid(2) + 25, green_info,'FontSize',14,'FontWeight','bold');
+    text(green_centre.Centroid(1) + 10, green_centre.Centroid(2),'GREEN','FontSize',14,'FontWeight','bold');
 
-scatter(yellow_centre.Centroid(1), yellow_centre.Centroid(2),'MarkerFaceColor','black','MarkerEdgeColor','black');
-yellow_u = num2str(round(yellow_centre.Centroid(1)));
-yellow_v = num2str(round(yellow_centre.Centroid(2)));
-yellow_info = append(yellow_u,", ",yellow_v);
-text(yellow_centre.Centroid(1) + 10, yellow_centre.Centroid(2) + 25, yellow_info,'FontSize',14,'FontWeight','bold');
-text(yellow_centre.Centroid(1) + 10, yellow_centre.Centroid(2),'yellow','FontSize',14,'FontWeight','bold');
+    % scatter(yellow_centre.Centroid(1), yellow_centre.Centroid(2),'MarkerFaceColor','black','MarkerEdgeColor','black');
+    % yellow_u = num2str(round(yellow_centre.Centroid(1)));
+    % yellow_v = num2str(round(yellow_centre.Centroid(2)));
+    % yellow_info = append(yellow_u,", ",yellow_v);
+    % text(yellow_centre.Centroid(1) + 10, yellow_centre.Centroid(2) + 25, yellow_info,'FontSize',14,'FontWeight','bold');
+    % text(yellow_centre.Centroid(1) + 10, yellow_centre.Centroid(2),'yellow','FontSize',14,'FontWeight','bold');
 
-scatter(purple_centre.Centroid(1), purple_centre.Centroid(2),'MarkerFaceColor','black','MarkerEdgeColor','black');
-purple_u = num2str(round(purple_centre.Centroid(1)));
-purple_v = num2str(round(purple_centre.Centroid(2)));
-purlpe_info = append(purple_u,", ",purple_v);
-text(purple_centre.Centroid(1) + 10, purple_centre.Centroid(2) + 25, purlpe_info,'FontSize',14,'FontWeight','bold');
-text(purple_centre.Centroid(1) + 10, purple_centre.Centroid(2),'purple','FontSize',14,'FontWeight','bold');
+    scatter(blue_centre.Centroid(1), blue_centre.Centroid(2),'MarkerFaceColor','black','MarkerEdgeColor','black');
+    blue_u = num2str(round(blue_centre.Centroid(1)));
+    blue_v = num2str(round(blue_centre.Centroid(2)));
+    purlpe_info = append(blue_u,", ",blue_v);
+    text(blue_centre.Centroid(1) + 10, blue_centre.Centroid(2) + 25, purlpe_info,'FontSize',14,'FontWeight','bold');
+    text(blue_centre.Centroid(1) + 10, blue_centre.Centroid(2),'BLUE','FontSize',14,'FontWeight','bold');
 
-% end
+end
 
 %% Shutdown
 
@@ -142,22 +143,21 @@ function [BW_object] = extract_yellow(image)
     BW_object = sliderBW;
 end
 
-function [BW_object] = extract_purple(image)
-%Isolate purple objects and return black and white image of isolated objects
+function [BW_object] = extract_blue(image)
     % Convert RGB image to chosen color space
     I = rgb2hsv(image);
 
     % Define thresholds for channel 1 based on histogram settings
-    channel1Min = 0.614;
-    channel1Max = 0.699;
+    channel1Min = 0.518;
+    channel1Max = 0.559;
 
     % Define thresholds for channel 2 based on histogram settings
-    channel2Min = 0.083;
-    channel2Max = 0.265;
+    channel2Min = 0.399;
+    channel2Max = 1.000;
 
     % Define thresholds for channel 3 based on histogram settings
-    channel3Min = 0.512;
-    channel3Max = 0.787;
+    channel3Min = 0.000;
+    channel3Max = 1.000;
 
     % Create mask based on chosen histogram thresholds
     sliderBW = (I(:,:,1) >= channel1Min ) & (I(:,:,1) <= channel1Max) & ...
